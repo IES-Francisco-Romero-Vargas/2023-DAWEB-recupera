5feb1ce26db4d11cfbb86f6fdf33dbc512b6e24b
angoies <jgomez@iesromerovargas.com>
2024-04-11 09:28:46 +0200

    elimino vars no usadas

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ def suma(a: int, b: int) -> int:
     """ docstring function
     # debe dar errores en linter
     """
-    y = "esta tampoco"
-    x = "no se usa"
+    # y = "esta tampoco"
+    # x = "no se usa"
     return a+ b
